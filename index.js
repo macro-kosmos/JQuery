274c85809cb7bf13691b890fece3c8d25477c2e6
IDkosmos <mr.sdeas@mail.ru>
2022-09-04 17:33:14 +0200

    effects

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
 // Swal.fire({ icon: "success" })
 // Swal.fire({ icon: "error" })
 // Swal.fire({ icon: "warning", text: "" })
+
+$(".box")
+	.effect("shake", { times: 3 }, 300)
+	.effect("highlight", {}, 1000)
+	.effect("explode", {}, 300)
