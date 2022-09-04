e64e33911267ab4b23709877ded66d70f65c13c2
IDkosmos <mr.sdeas@mail.ru>
2022-09-04 18:02:37 +0200

    scroll

--- index.js
+++ index.js
@@ -2,7 +2,10 @@
 // Swal.fire({ icon: "error" })
 // Swal.fire({ icon: "warning", text: "" })
 
-$(".box")
-	.effect("shake", { times: 3 }, 300)
-	.effect("highlight", {}, 1000)
-	.effect("explode", {}, 300)
+$(".box").scroll(function () {
+	$("body").append(
+		$("<div>", {
+			text: "Hello",
+		})
+	)
+})
