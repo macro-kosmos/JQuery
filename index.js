6dae2896c31b1648eed92499385e43652b9338dd
IDkosmos <mr.sdeas@mail.ru>
2022-09-05 00:11:33 +0200

    scroll top

--- index.js
+++ index.js
@@ -2,12 +2,7 @@
 // Swal.fire({ icon: "error" })
 // Swal.fire({ icon: "warning", text: "" })
 
-let nav = $(".nav")
-
-$(window).scroll(function () {
-	if (!nav.hasClass("fixed") && $(window).scrollTop() > nav.offset().top) {
-		nav.addClass("fixed").data("top", nav.offset().top)
-	} else if (nav.hasClass("fixed") && $(window).scrollTop() < nav.data("top")) {
-		nav.removeClass("fixed")
-	}
+$("a[href='#']").click(function (event) {
+	event.preventDefault()
+	$("body, html").animate({ scrollTop: 0 }, "slow")
 })
